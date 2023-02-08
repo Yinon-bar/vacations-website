f314dc17bf18aa59061a8fd0a720e40c399b2be2
Yinon bar <yinonbar1988@gmail.com>
2023-02-08 15:40:29 +0200

    image working

--- vacations_task.sql
+++ vacations_task.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2023 at 06:22 AM
+-- Generation Time: Feb 08, 2023 at 02:39 PM
 -- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -73,8 +73,9 @@ CREATE TABLE `vacations` (
 
 INSERT INTO `vacations` (`id`, `description`, `destenation`, `image`, `start_date`, `end_date`, `price`, `folowers`) VALUES
 (1, 'miami vacation', 'miami', '', '28/01/2023', '05/02/2023', 2800, 4),
-(6, 'hawai vacation', 'hawai', 'hawaii.jpg', '05/02/2023', '15/02/2023', 6300, 16),
-(7, 'Jerusalem - israel', 'come see Jerusalem in the mirror of time', '', '20/03/2023', '27/03/2023', 2500, 50);
+(6, 'hawai vacation', 'hawai', '', '05/02/2023', '15/02/2023', 6300, 16),
+(7, 'Jerusalem - israel', 'come see Jerusalem in the mirror of time', '', '20/03/2023', '27/03/2023', 2500, 50),
+(19, 'Moroco - dessert and sea', 'Marakesh', 'c16a8aec-0e98-4b14-8ad6-f5ea437ec757.jpg', '05/02/2023', '15/02/2023', 2100, 32);
 
 --
 -- Indexes for dumped tables
@@ -106,7 +107,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `vacations`
 --
 ALTER TABLE `vacations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
